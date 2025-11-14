cca7b4e2daf1c7523eb48c6c26f45ee7da07ac89
Ranjith BR <ranjith19@gmail.com>
2025-11-14 10:18:56 +0530

    change name of span

--- src/otel_fastapi_link_middleware/__init__.py
+++ src/otel_fastapi_link_middleware/__init__.py
@@ -4,7 +4,7 @@ from opentelemetry.trace import Link
 from starlette.types import ASGIApp, Receive, Scope, Send
 
 
-def get_otlp_tracer(name: str) -> trace.Tracer:
+def _get_otlp_tracer(name: str) -> trace.Tracer:
     """Get a tracer for the given name
 
     Args:
@@ -15,7 +15,7 @@ def get_otlp_tracer(name: str) -> trace.Tracer:
     return trace.get_tracer(name)
 
 
-def get_current_otlp_span() -> trace.Span:
+def _get_current_otlp_span() -> trace.Span:
     """Get the current active span
 
     Returns:
@@ -24,7 +24,7 @@ def get_current_otlp_span() -> trace.Span:
     return trace.get_current_span()
 
 
-def copy_span_attributes(src_span: trace.Span, dest_span: trace.Span) -> None:
+def _copy_span_attributes(src_span: trace.Span, dest_span: trace.Span) -> None:
     # src_span._attributes is safe; it’s part of the SDK span implementation
     for k, v in src_span._attributes.items():  # type: ignore[attr-defined]
         dest_span.set_attribute(k, v)
@@ -62,20 +62,23 @@ class LinkedTraceMiddleware:
     async def _link_trace(self, scope: Scope, receive: Receive, send: Send) -> None:
         parent_ctx = trace.get_current_span().get_span_context()
         new_context = otel_context.Context()
-        current_span = get_current_otlp_span()
+        current_span = _get_current_otlp_span()
         span_name = current_span.name  # type: ignore[attr-defined]
-        new_name = f"ApiLinkSpan | {span_name}"
+        new_name = "LinkedApiSpan"
+        current_span.set_attribute("linked_trace", True)
+        current_span.set_attribute("url.path", "" + scope.get("path", ""))
+
         current_span.update_name(new_name)
-        tracer = get_otlp_tracer(__name__)
+        tracer = _get_otlp_tracer(__name__)
 
         with tracer.start_as_current_span(
             span_name,
             context=new_context,  # start new trace
             links=[Link(parent_ctx)] if parent_ctx.is_valid else None,
         ):
-            child_span = get_current_otlp_span()
+            child_span = _get_current_otlp_span()
             child_span_context = child_span.get_span_context()
-            copy_span_attributes(current_span, child_span)
+            _copy_span_attributes(current_span, child_span)
             current_span.add_link(child_span_context)
 
             await self.app(scope, receive, send)
